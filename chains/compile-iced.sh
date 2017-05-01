3af87313d539f292a4d29319054f3b189c061f0c
Maxwell Krohn <themax@gmail.com>
2017-05-01 17:37:04 -0400

    wip

--- chains/compile-iced.sh
+++ chains/compile-iced.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-input=$1
-output=`basename -s .iced $1`.json
-forge-sigchain -f iced < $input | json_pp > $output
+for i in $*; do
+	input=$i
+	output=`basename -s .iced $1`.json
+	forge-sigchain -f iced < $input | json_pp > $output
+done
 
