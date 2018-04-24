260b815592d65ec410ecf2e1d6351d8ef0e2db54
Maxwell Krohn <themax@gmail.com>
2018-04-24 11:46:34 -0400

    test cases for unsupported chain links

--- chains/compile-iced.sh
+++ chains/compile-iced.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
+args=`getopt s $*`
+if [ $? -ne 0 ]
+then
+    usage
+fi
+
+PRETTY=json_pp
+
+for i
+do
+	case "$i"
+	in
+		-s)
+			PRETTY=cat
+			shift
+			;;
+		--)
+          shift ; break
+			;;
+	esac
+done
+
 for i in $*; do
 	input=$i
-	output=`basename -s .iced $1`.json
-	forge-sigchain -f iced < $input | json_pp > $output
-    #forge-sigchain -f iced < $input > $output
+	output=`basename -s .iced $i`.json
+	forge-sigchain -f iced < $input | $PRETTY > $output
 done
 
